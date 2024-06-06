d0a36e5a351772e7b6ad45c1cc6987e441d21da4
gwachukwu <gwachukwuelebeke@gmail.com>
2024-06-05 21:32:55 -0400

    Keep it simple

--- MultiCalculator/shared/src/commonMain/kotlin/Calculator.kt
+++ MultiCalculator/shared/src/commonMain/kotlin/Calculator.kt
@@ -5,18 +5,15 @@ class Calculator {
         return left + right
     }
 
-    fun subtract(left: Int, right: Int): Int {
+    fun Subtract(left: Int, right: Int): Int {
         return left - right
     }
 
-    fun multiply(left: Int, right: Int): Int {
+    fun Multiply(left: Int, right: Int): Int {
         return left * right
     }
 
-    fun divide(left: Int, right: Int): Int {
-        if (left == 0) {
-            throw IllegalArgumentException("Division by zero is not allowed")
-        }
-        return left / right
+    fun Divide(left: Int, right: Int): Int {
+       return left / right
     }
 }
