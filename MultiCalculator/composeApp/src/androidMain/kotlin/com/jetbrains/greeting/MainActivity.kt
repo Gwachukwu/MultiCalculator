fa715c9fafb4e117dd60fdef9384359bd86b1a45
gwachukwu <gwachukwuelebeke@gmail.com>
2024-06-12 16:57:51 -0400

    Implement CalcEqualsButton

--- MultiCalculator/composeApp/src/androidMain/kotlin/com/jetbrains/greeting/MainActivity.kt
+++ MultiCalculator/composeApp/src/androidMain/kotlin/com/jetbrains/greeting/MainActivity.kt
@@ -4,8 +4,14 @@ import App
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,4 +33,34 @@ fun AppAndroidPreview() {
 @Composable
 fun CalcView() {
 
+}
+
+@Composable
+fun CalcRow() {
+
+}
+
+@Composable
+fun CalcDisplay() {
+
+}
+
+@Composable
+fun CalcNumericButton() {
+
+}
+
+@Composable
+fun CalcOperationButton(operation: String, display: MutableState<String>) {
+
+}
+
+@Composable
+fun CalcEqualsButton(display: MutableState<String>) {
+    Button(
+        onClick = { display.value = "0" },
+        modifier = Modifier.padding(4.dp)
+        ) {
+        Text("=")
+    }
 }
